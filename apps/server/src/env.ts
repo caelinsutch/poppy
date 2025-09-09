5dfa6ed8bb2dc122c5653b047fd6b1c21a315842
Caelin Sutch <caelinsutch@gmail.com>
2025-09-09 18:17:35 -0400

    Coerce

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const env = createEnv({
   server: {
-    PORT: z.number().optional().default(3000),
+    PORT: z.coerce.number().optional().default(3000),
     HOST: z.string().optional().default('0.0.0.0'),
     LOOP_AUTHORIZATION_KEY: z.string().min(1),
     LOOP_SECRET_KEY: z.string().min(1),
