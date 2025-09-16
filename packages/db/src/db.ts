31812116258a21d5fb5e84371dcc4b20e6284ef2
Caelin Sutch <caelinsutch@gmail.com>
2025-09-16 15:31:21 -0400

    Disable prepare

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -8,6 +8,7 @@ config({ path: ".env" });
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
+const connectionString = process.env.DATABASE_URL
 
-const client = postgres(process.env.DATABASE_URL);
+const client = postgres(connectionString, { prepare: false })
 export const db = drizzle(client, { schema });
